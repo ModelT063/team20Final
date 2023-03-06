e47ce2a961f6a348989f44fea4e18ca1a17f3593
Joel Pittard <jpittar@clemson.edu>
2023-03-06 16:35:22 -0500

    moved aws exports file

--- team20/src/pages/_index.tsx
+++ team20/src/pages/_index.tsx
@@ -1,18 +1,18 @@
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 
-import { Amplify } from 'aws-amplify';
+import { Amplify } from "aws-amplify";
 
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-import awsExports from './aws-exports-fixed';
+import awsExports from "../../aws-exports-fixed";
 Amplify.configure(awsExports);
 
-function App({ signOut, user }: {signOut: any; user: any}) {
+function App({ signOut, user }: { signOut: any; user: any }) {
   return (
     <>
       <div>
-        <Navbar/>
+        <Navbar />
         <button onClick={signOut}>Sign out</button>
       </div>
       <h1>HOMEPAGE!!</h1>
