852bdce0dd05956860fc2232e91af803349a8e2c
zlc4 <78279322+zlc4@users.noreply.github.com>
2023-03-13 00:30:05 -0400

    Made additions to the page

--- team20/src/pages/catalog.tsx
+++ team20/src/pages/catalog.tsx
@@ -1,11 +1,19 @@
 import Navbar from '../components/Navbar';
-export default function Catalog() {
+
+export default function Account() {
     return (
         <>
         <div>
             <Navbar/>
         </div>
         <h1>Catalog</h1>
+        <table>
+            <th> Product ID </th>
+            <th> Name       </th>
+            <th> Price      </th> 
+            <th> Description</th>            
+        </table>
         </>
     );
 }
+
