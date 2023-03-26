0f7cb943117957728e46a144d322947b2b601b6d
abodell <andybodell9@gmail.com>
2023-03-26 15:15:11 -0400

    Fixes for catalog type and API calls to support the storage of JSON data for the catalog items

--- team20/src/pages/api/catalog/create/addcatalog.ts
+++ team20/src/pages/api/catalog/create/addcatalog.ts
@@ -12,10 +12,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const newCatalog: CatalogSettings = req.body;
         db.connect( (err) => {
             if (err) throw err;
-            db.query("INSERT INTO CatalogSettings VALUES (?, ?)", 
+            db.query("INSERT INTO CatalogSettings VALUES (?, ?, ?)", 
             [
                 0,
                 newCatalog.Catalog_Name,
+                '{}',
             ], (error: any, results: any, fields: any) => {
                 if (error) throw error;
                 return res.status(200).json(results);
--- team20/src/pages/api/catalog/update/[id].ts
+++ team20/src/pages/api/catalog/update/[id].ts
@@ -10,11 +10,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         console.log(req.body);
         const updatedCatalog: CatalogSettings = req.body;
+        console.log(updatedCatalog.iTunes_Endpoint);
         db.connect( (err) => {
             if (err) throw err;
-            db.query("UPDATE CatalogSettings SET Catalog_Name = ? WHERE Catalog_ID = ?", 
+            db.query("UPDATE CatalogSettings SET Catalog_Name = ?, iTunes_Endpoint = ? WHERE Catalog_ID = ?", 
             [
                 updatedCatalog.Catalog_Name,
+                JSON.stringify(updatedCatalog.iTunes_Endpoint),
                 parseInt(req.query.id as string)
             ], (error: any, results: any, fields: any) => {
                 if (error) throw error;
