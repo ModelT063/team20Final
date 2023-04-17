27ed8da357826df66c3cb6c6db2b6488ce756a9d
abodell <andybodell9@gmail.com>
2023-04-17 13:24:39 -0400

    Resolving merge conflict

--- team20/src/pages/pii.tsx
+++ team20/src/pages/pii.tsx
@@ -1,14 +1,73 @@
 import Navbar from "../components/Navbar";
-import { CircularProgress } from "@mui/material";
-import { userInfoState, userOrganizations } from "@/lib/userData";
+import { Alert, Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, TextField } from "@mui/material";
+import { userID, userInfoState, userOrganizations } from "@/lib/userData";
 import { useRecoilValue, useRecoilState } from "recoil";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getOrgs } from "@/utils/userService";
+import { Close, Edit } from "@mui/icons-material";
 
 export default function pii() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [info, setInfo] = useRecoilState(userInfoState);
+
   let userInfo = useRecoilValue(userInfoState);
+  let id = useRecoilValue(userID);
   let orgs = useRecoilValue(userOrganizations)
-  console.log(orgs);
+
+  const titleStyles = {
+    display: "flex",
+    justifyContent: "space-between"
+  };
+
+  const alertStyles = {
+    marginTop: "1rem"
+  }
+
+  const actionStyles = {
+    justifyContent: "center",
+    alignItems: 'center',
+    flexDirection: 'column' as 'column',
+  };
+
+  const handleOpen = () => {
+    setIsOpen(true);
+  }
+
+  const handleClose = () => {
+    setIsOpen(false);
+  }
+  
+  const handleSubmit = () => {
+    if (email == '' || firstName == '' || lastName == '') {
+      setIsError(true);
+      return;
+    }
+    let updatedUser = {
+      Email: email,
+      User_Type: userInfo[0]['User_Type'],
+      User_Status: userInfo[0]['User_Status'],
+      F_Name: firstName,
+      L_Name: lastName,
+      Points: userInfo[0]['Points'],
+      Cart: {
+        ids: userInfo[0]['Cart']
+      }
+    }
+    setInfo([updatedUser] as any)
+    fetch(`http://localhost:3000/api/users/update/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedUser)
+    });
+    setIsUpdated(true);
+  }
   return (userInfo[0] == undefined ? <CircularProgress/> :
     <>
       <div>
@@ -31,6 +90,55 @@ export default function pii() {
           <td>{orgs.length > 0  ?  orgs.map((org) => <li>{org['Organization_Name']}</li>) : "You don't belong to any organizations"}</td>
         </tr>
         </table></center>
+        {userInfo[0] && <IconButton onClick = {handleOpen} children = {<Edit />}/>}
+      <Dialog open={isOpen}>
+        <DialogTitle style = {titleStyles}>Update Information
+          <IconButton children = {<Close/>} onClick = {handleClose}/>
+        </DialogTitle>
+        <DialogContent>
+          <TextField 
+            autoFocus
+            margin="dense"
+            id="firstname"
+            label="First Name"
+            type="text"
+            fullWidth
+            variant="standard"
+            required
+            onChange={e => {
+              setFirstName(e.target.value)
+            }}
+          />
+          <TextField 
+            margin="dense"
+            id="lastname"
+            label="Last Name"
+            type="text"
+            fullWidth
+            variant="standard"
+            required
+            onChange={e => {
+              setLastName(e.target.value)
+            }}
+          />
+          <TextField 
+            margin="dense"
+            id="email"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+            required
+            onChange={e => {
+              setEmail(e.target.value)
+            }}
+          />
+        </DialogContent>
+        <DialogActions style={actionStyles}>
+          <Button onClick={handleSubmit} variant = 'contained'>Save</Button>
+          {isUpdated ? <Alert style = {alertStyles} severity = 'success'>Your Information was Updated!</Alert> : isError ?  <Alert style = {alertStyles} severity = 'error'>Please fill out each field</Alert> : null}
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
