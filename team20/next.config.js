26a75c8878f56ca7270730627a7ae3484e7ba2a9
ModelT063 <jpittar@clemson.edu>
2023-04-23 20:04:45 -0400

    changed env variables

--- team20/next.config.js
+++ team20/next.config.js
@@ -14,18 +14,24 @@ module.exports = (phase) => {
           REDIRECT_SIGN_IN: "http://localhost:3000/token",
           REDIRECT_SIGN_OUT: "http://localhost:3000/",
           AUTH_COOKIE_DOMAIN: "localhost",
+          DATABASE_URL: process.env.DATABASE_URL,
+          DATABASE_USER: process.env.DATABASE_USER,
+          DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+          DATABASE_PORT: process.env.DATABASE_PORT,
+          DATABASE_NAME: process.env.DATABASE_NAME,
+          APP_URL: "http://localhost:3000",
         },
       };
     default:
       return {
         // environment varibales for production
         env: {
-          IDP_DOMAIN: " ",
-          USER_POOL_ID: " ",
-          USER_POOL_CLIENT_ID: " ",
-          REDIRECT_SIGN_IN: " ",
-          REDIRECT_SIGN_OUT: " ",
-          AUTH_COOKIE_DOMAIN: " ",
+          IDP_DOMAIN: "https://team20-truckdrivers.auth.us-east-1.amazoncognito.com",
+          USER_POOL_ID: "us-east-1_uXeFvCqps",
+          USER_POOL_CLIENT_ID: "39tpk87ra9174ka9vv6trla406", 
+          REDIRECT_SIGN_IN: process.env.APP_URL + "/token",
+          REDIRECT_SIGN_OUT: process.env.APP_URL,
+          AUTH_COOKIE_DOMAIN: "main.d36uuflbr4swes.amplifyapp.com",
           DATABASE_URL: process.env.DATABASE_URL,
           DATABASE_USER: process.env.DATABASE_USER,
           DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
